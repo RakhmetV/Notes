50a2a93e500abb6aebdb861bef8a99cf5ef63d86
RakhmetV <108589498+RakhmetV@users.noreply.github.com>
2023-08-08 22:58:30 +0500

    work 11

--- Notes.WebApi/Conrtollers/NoteController.cs
+++ Notes.WebApi/Conrtollers/NoteController.cs
@@ -33,9 +33,6 @@ namespace Notes.WebApi.Controllers
         /// <response code="200">Success</response>
         /// <response code="401">If the user is unauthorized</response>
         [HttpGet]
-        [Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteListVm>> GetAll()
         {
             var query = new GetNoteListQuery
@@ -58,9 +55,6 @@ namespace Notes.WebApi.Controllers
         /// <response code="200">Success</response>
         /// <response code="401">If the user in unauthorized</response>
         [HttpGet("{id}")]
-        [Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
         {
             var query = new GetNoteDetailsQuery
